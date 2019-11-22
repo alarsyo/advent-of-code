d17e26540f9a994a216e731d8b2f4c034a216357
Antoine Martin <antoine97.martin@gmail.com>
2019-11-22 17:00:32 +0100

    2018: day05: part 1

---
+++ aoc2018/src/day05.rs
@@ -0,0 +1,63 @@
+use super::Result;
+
+const INPUT: &str = include_str!("../input/day05.txt");
+
+pub fn run() -> Result<()> {
+    println!("part 1: {}", part1(INPUT)?);
+
+    Ok(())
+}
+
+fn same_type(a: char, b: char) -> bool {
+    a.to_ascii_lowercase() == b.to_ascii_lowercase()
+}
+
+fn part1(input: &str) -> Result<usize> {
+    let mut res = String::with_capacity(input.len());
+
+    // tracks last character of string
+    let mut last: Option<char> = None;
+
+    for next in input.chars() {
+        match last {
+            Some(elem) => {
+                // if same type but different polarity
+                if same_type(elem, next) && elem != next {
+                    // drop both elem and next
+                    last = res.pop();
+                } else {
+                    // consider elem "safe" to push
+                    res.push(elem);
+                    last = Some(next);
+                }
+            }
+            None => {
+                last = Some(next);
+            }
+        }
+    }
+
+    // add last character to string if exists
+    if let Some(c) = last {
+        res.push(c);
+    }
+
+    Ok(res.len())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const PROVIDED: &str = "dabAcCaCBAcCcaDA";
+
+    #[test]
+    fn part1_provided() {
+        assert_eq!(part1(PROVIDED).unwrap(), 10);
+    }
+
+    #[test]
+    fn part1_real() {
+        assert_eq!(part1(INPUT).unwrap(), 10638);
+    }
+}
--- aoc2018/src/main.rs
+++ aoc2018/src/main.rs
@@ -3,11 +3,12 @@ use std::env;
 use aoc2018::day01;
 use aoc2018::day02;
 use aoc2018::day03;
+use aoc2018::day05;
 
 use aoc2018::Result;
 
 fn main() -> Result<()> {
-    let days: &[fn() -> Result<()>] = &[day01::run, day02::run, day03::run];
+    let days: &[fn() -> Result<()>] = &[day01::run, day02::run, day03::run, day05::run];
 
     let mut args = env::args();
     args.next();
