a5911369e5244e90cbd170bae2a21dd0f97293ef
Antoine Martin <antoine97.martin@gmail.com>
2019-12-03 15:40:50 +0100

    2015: day04: part 2

--- aoc2015/src/day04.rs
+++ aoc2015/src/day04.rs
@@ -7,6 +7,7 @@ const INPUT: &str = include_str!("../input/day04.txt");
 
 pub fn run() -> Result<()> {
     println!("part 1: {}", part1(INPUT)?);
+    println!("part 2: {}", part2(INPUT)?);
 
     Ok(())
 }
@@ -31,6 +32,26 @@ fn part1(input: &str) -> Result<u64> {
     Err(err!("couldn't find a suitable number"))
 }
 
+fn part2(input: &str) -> Result<u64> {
+    let input = input.trim_end();
+    let mut content = String::from(input);
+
+    let mut hasher = Md5::new();
+
+    for i in 0.. {
+        content.truncate(input.len());
+        content.extend(i.to_string().chars());
+
+        hasher.input(&content);
+        let res = hasher.result_reset();
+        if &res[..3] == &[0, 0, 0] {
+            return Ok(i);
+        }
+    }
+
+    Err(err!("couldn't find a suitable number"))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -46,5 +67,11 @@ mod tests {
     #[ignore] // takes too long!
     fn part1_real() {
         assert_eq!(part1(INPUT).unwrap(), 282749);
+    }
+
+    #[test]
+    #[ignore] // takes too long!
+    fn part2_real() {
+        assert_eq!(part2(INPUT).unwrap(), 9962624);
     }
 }
