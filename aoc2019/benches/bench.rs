0c98050bc08a146c3072f44f132b84eb4bd9e7a5
Antoine Martin <antoine97.martin@gmail.com>
2019-12-06 19:07:57 +0100

    2019: add benchmarks

---
+++ aoc2019/benches/bench.rs
@@ -0,0 +1,24 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+
+use aoc2019::day01;
+use aoc2019::day02;
+use aoc2019::day03;
+use aoc2019::day04;
+use aoc2019::day05;
+use aoc2019::day06;
+
+fn aoc2019_all(c: &mut Criterion) {
+    c.bench_function("day01", |b| b.iter(|| day01::run().unwrap()));
+    c.bench_function("day02", |b| b.iter(|| day02::run().unwrap()));
+    c.bench_function("day03", |b| b.iter(|| day03::run().unwrap()));
+    c.bench_function("day04", |b| b.iter(|| day04::run().unwrap()));
+    c.bench_function("day05", |b| b.iter(|| day05::run().unwrap()));
+    c.bench_function("day06", |b| b.iter(|| day06::run().unwrap()));
+}
+
+criterion_group! {
+    name = all_days;
+    config = Criterion::default().sample_size(10);
+    targets = aoc2019_all
+}
+criterion_main!(all_days);
