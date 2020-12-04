13420d518a2b330318b47494f2558fb72f039fa8
Antoine Martin <antoine97.martin@gmail.com>
2020-12-04 10:58:35 +0100

    2020: day04: bench

--- aoc2020/benches/bench.rs
+++ aoc2020/benches/bench.rs
@@ -3,11 +3,13 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use aoc2020::day01;
 use aoc2020::day02;
 use aoc2020::day03;
+use aoc2020::day04;
 
 fn aoc2020_all(c: &mut Criterion) {
     c.bench_function("day01", |b| b.iter(|| day01::run().unwrap()));
     c.bench_function("day02", |b| b.iter(|| day02::run().unwrap()));
     c.bench_function("day03", |b| b.iter(|| day03::run().unwrap()));
+    c.bench_function("day04", |b| b.iter(|| day04::run().unwrap()));
 }
 
 criterion_group! {
