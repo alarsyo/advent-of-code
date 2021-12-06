7cffb7cdc6315954a8394083129ea3e7d44b0a9c
Antoine Martin <antoine@alarsyo.net>
2021-12-06 11:25:08 +0100

    2021: day06: bench

--- aoc2021/aoc2021_bench/benches/aoc2021_bench.rs
+++ aoc2021/aoc2021_bench/benches/aoc2021_bench.rs
@@ -5,6 +5,7 @@ use aoc2021::day02;
 use aoc2021::day03;
 use aoc2021::day04;
 use aoc2021::day05;
+use aoc2021::day06;
 
 fn aoc2021_all(c: &mut Criterion) {
     c.bench_function("day01", |b| b.iter(|| day01::run().unwrap()));
@@ -12,6 +13,7 @@ fn aoc2021_all(c: &mut Criterion) {
     c.bench_function("day03", |b| b.iter(|| day03::run().unwrap()));
     c.bench_function("day04", |b| b.iter(|| day04::run().unwrap()));
     c.bench_function("day05", |b| b.iter(|| day05::run().unwrap()));
+    c.bench_function("day06", |b| b.iter(|| day06::run().unwrap()));
 }
 
 criterion_group! {
